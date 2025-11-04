af62a9d6691dc5f3c0f9a2e65843318b60c34df9
Harshit-jain-786 <159174925+Harshit-jain-786@users.noreply.github.com>
2025-11-05 00:00:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/TravelIndia/" : "/",
+  base: "/", // ✅ Render only
   plugins: [react()],
   resolve: {
     alias: {
